bd0aa4c3c4bfcbcea10d6716ad86dfe24b0cb716
Дарья Провальская <kaamomee@gmail.com>
2024-10-30 22:07:22 +0300

    code without using jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 const currentYear = new Date().getFullYear(); //декларативный
 
 export const App = () => {
-	//императивный
-	return (
-		//декларативный
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		React.createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				React.createElement('code', null, 'src/App.js'),
+				'and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('p', null, currentYear),
+		),
 	);
 };
